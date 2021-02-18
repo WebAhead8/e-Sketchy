8983c48fabeef5d39a7c2af567b9564fe150b824
BoshEee <bosheee.shwi@gmail.com>
2021-02-18 12:25:56 +0200

    update checkout form

--- e-sketchy/src/components/CheckoutForm.js
+++ e-sketchy/src/components/CheckoutForm.js
@@ -1,23 +1,29 @@
 import React from "react";
 import "../style/Form.css";
 
+function thanksMsg() {
+  alert("Thanks, The team will ship your order soon.");
+}
 function CheckoutForm(props) {
   return (
     <div className="cont">
       <form className="form">
         <label> Type Your Payment Information</label>
         <label>Full Name :</label>
-        <input type="text" placeholder="name" />
+        <input type="text" placeholder="name" require />
         <label>Card Number :</label>
-        <input type="number" paceholder="CardNumber" />
+        <input type="number" paceholder="CardNumber" require />
         <label>CVV :</label>
-        <input type="number" placeholder="cvv" />
+        <input type="number" placeholder="cvv" require />
         <label>Email :</label>
-        <input type="email" placeholder="Email" />
+        <input type="email" placeholder="Email" require />
         <label>Address :</label>
-        <input type="text" placeholder="City" />
-        <input type="number" placeholder="PostalCode" />
-        <button type="submit"> Confirm Your Payment </button>
+        <input type="text" placeholder="City" require />
+        <input type="number" placeholder="PostalCode" require />
+        <button type="submit" onClick={thanksMsg}>
+          {" "}
+          Confirm Your Payment{" "}
+        </button>
       </form>
     </div>
   );
